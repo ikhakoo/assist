827776b0226d4c4c0b44477bea814fb64802c03b
ikhakoo <ikhakoo@gmail.com>
2015-04-27 11:22:09 -0400

    creating user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
-  def create
+  def new
+    @user = User.new
   end
 
-  def new
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Signed up!"
+    else
+      render "new"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_type, :first_name, :last_name, :email, :address, :city, :state, :country, :password, :password_confirmation)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427141148) do
+ActiveRecord::Schema.define(version: 20150427150034) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -23,8 +23,10 @@ ActiveRecord::Schema.define(version: 20150427141148) do
     t.string   "state"
     t.string   "country"
     t.string   "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "user_type"
+    t.string   "password_digest"
   end
 
 end
